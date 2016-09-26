46418c27747bb4d5289d3a8b0affb45fe31b3d3f
kkimu <kimura.kzm@gmail.com>
2016-09-27 02:56:16 +0900

    :shower: Delete postgres container & wercher.yml, and Clean

--- setup.sh
+++ setup.sh
@@ -4,26 +4,15 @@ set -ex
 echo "------------ start setup.sh --------------"
 
 
-mkdir -p $PWD/static
+mkdir -p $PWD/static #staticファイルの置き場所
+
+# アプリのデータコンテナ 起動していないときだけ起動する
 da=`docker ps -f name=data-app -aq`
 if [ -z "${da}" ]; then
 	docker run --name data-app -v $PWD/static:/static busybox
 fi
 
-my=`docker ps -f name=postgres -q`
-if [ -z "${my}" ]; then
-	docker build -t postgres:0.1 ./postgres
-
-	docker run \
-		--name postgres \
-		-e POSTGRES_PASSWORD=$DB_PASS \
-		-d \
-		-p 5432:5432 \
-		postgres:0.1
-
-fi
-
-
+# Nginxのコンテナ 起動していないときだけ起動する
 ng=`docker ps -f name=nginx -q`
 if [ -z "${ng}" ]; then
 	docker build -t nginx:0.1 ./nginx
@@ -35,7 +24,7 @@ if [ -z "${ng}" ]; then
 		nginx:0.1
 fi
 
-
+# 画像処理部分のコンテナ deployの度に再起動
 ie=`docker ps -f name=image_engine -q`
 if [ -n "${ie}" ]; then
 	docker stop image_engine
@@ -51,6 +40,7 @@ docker run \
 	image_engine:0.1
 
 
+# railsアプリのコンテナ deployの度に再起動
 sp=`docker ps -f name=selp -q`
 if [ -n "${sp}" ]; then
 	docker stop selp
@@ -60,17 +50,13 @@ docker build -t selp:0.1 ./app
 
 docker run \
 	--name selp \
-	--link postgres \
 	--link image_engine \
 	--volumes-from data-app \
-	-e VIRTUAL_HOST=$VIRTUAL_HOST \
-	-e DB_PASS=$DB_PASS \
 	-e RAILS_ENV=$ENV \
 	-e RACK_ENV=$ENV \
 	-d \
 	-p 3000:3000 \
 	selp:0.1
-
 
 
 
